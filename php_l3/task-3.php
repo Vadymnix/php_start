591ba94c1a8e6c6fb726794054016882ec95efe9
chernomor <vb.it.pro.service@gmail.com>
2022-09-22 22:08:37 +0300

    finished task-3 lesson-3. All task finished

--- php_l3/task-3.php
+++ php_l3/task-3.php
@@ -2,7 +2,7 @@
 $students = [
     'ИТ20' => [
         'Иванов Петр' => 5,
-        'Кириллов Кирилл' => 1,
+        'Киряллов Кирилл' => 1,
         'Иванов Василий' => 5,
         'Петров Иван' => 3,
         'Петров Кирилл' => 2,
@@ -11,8 +11,45 @@ $students = [
     'БАП20' => [
         'Антонов Антон' => 4,
         'Иванов Иван' => 5,
-        'Васечкин Иван' => 3,
-        'Залужный Владимир' => 3,
+        'Васечкин Иван' => 5,
+        'Залужный Владимир' => 4,
+        'Карякин Петр' => 2,
+    ],
+    'УУ10' => [
+        'Анто Антон' => 4,
+        'Ванька Иван' => 1,
+        'Васька Иван' => 2,
+        'Макаревич Андрей' => 4,
         'Карякин Петр' => 2,
     ]
 ];
+$maxAverageValue = [];
+$listBye = [];
+
+foreach($students as $nameGroup => $group) {
+    $currentValue = 0;
+    $countStudent = count($group);
+
+    foreach ($group as $nameStudent => $value) {
+        $currentValue += $value;
+        if($value < 3) {
+            $listBye[$nameGroup][$nameStudent] = $value;
+        }
+    }
+
+    $averageValue = $currentValue/$countStudent;
+    if(empty($maxAverageValue)) {
+        $maxAverageValue[$nameGroup] = $averageValue;
+    } else {
+        $currentKey = key($maxAverageValue);
+        if ($averageValue  > $maxAverageValue[$currentKey]) {
+                unset($maxAverageValue[$currentKey]);
+                $maxAverageValue[$nameGroup] = $averageValue ;
+        }
+    }
+}
+
+echo "Average value: \n";
+print_r($maxAverageValue);
+echo "Bye list of students: \n";
+print_r($listBye);
