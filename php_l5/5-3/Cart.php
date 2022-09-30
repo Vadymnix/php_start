301cd8b722b69c37a3524886cab7c537f03d7678
chernomor <vb.it.pro.service@gmail.com>
2022-09-30 22:20:03 +0300

    finished working day

--- php_l5/5-3/Cart.php
+++ php_l5/5-3/Cart.php
@@ -2,5 +2,63 @@
 class Product {
     private string $title;
     private float $price;
-    private Product $components;
+    private array $components = [];
+
+    /**
+     * @param string $title
+     * @param float $price
+     */
+    public function __construct(string $title, float $price)
+    {
+        $this->title = $title;
+        $this->price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getComponents(): Product
+    {
+        return $this->components;
+    }
+
+    /**
+     * @param Product $components
+     */
+    public function setComponents(Product $components): void
+    {
+        $this->components = $components;
+    }
 }
