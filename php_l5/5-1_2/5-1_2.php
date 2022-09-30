e0daf3d85a246c3dbfa8123ee3cde1a00f527767
chernomor <vb.it.pro.service@gmail.com>
2022-09-30 21:15:28 +0300

    finished 5-2

--- php_l5/5-1_2/5-1_2.php
+++ php_l5/5-1_2/5-1_2.php
@@ -1,6 +1,8 @@
 <?php
 require_once ("Task.php");
 require_once ("User.php");
+require_once ('Comment.php');
+require_once ("TaskService.php");
 
 $user = new User("Вася", "wqeq@dd.com", "male");
 $task = new Task($user, "vyjujsfdsfds;j;lsdf буквы много", 2);
@@ -15,3 +17,9 @@ echo "\nDescription " . $task->getDescription() . " TIME: " .
 $task->setDescription("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY\n");
 echo "\nDescription " . $task->getDescription() . " TIME: " .
     $task->getDateUpdated()->format("l d-m-Y h:i:s:u") . "\n";
+
+$commentString = "Hello, I am Vasya";
+
+TaskService::addComment($user, $task, $commentString);
+echo "Comments\n-----------------------------------------\n";
+var_dump($task->getComments());
